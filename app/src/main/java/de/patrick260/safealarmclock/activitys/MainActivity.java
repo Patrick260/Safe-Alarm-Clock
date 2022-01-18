797392e6735ef64ad33ca03879faec71fa740a89
Patrick260 <Patrick260@protonmail.com>
2022-01-18 18:57:10 +0100

    Add basic gui features

---
+++ app/src/main/java/de/patrick260/safealarmclock/activitys/MainActivity.java
@@ -0,0 +1,19 @@
+package de.patrick260.safealarmclock.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.patrick260.safealarmclock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
