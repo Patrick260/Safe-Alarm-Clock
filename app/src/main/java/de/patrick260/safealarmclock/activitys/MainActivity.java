32d749344b647f30901a693a6374e0bf7c97342a
Patrick260 <Patrick260@protonmail.com>
2022-02-20 14:00:51 +0100

    Refactor MainActivity.java

--- app/src/main/java/de/patrick260/safealarmclock/activitys/MainActivity.java
+++ app/src/main/java/de/patrick260/safealarmclock/activitys/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public void scheduleAlarm(View view) {
 
         startActivity(new Intent(this, ScheduleAlarmActivity.class));
