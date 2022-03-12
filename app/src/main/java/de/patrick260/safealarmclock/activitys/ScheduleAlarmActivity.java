57fb302ba38b4be6e3a678aae1b395a1279340b5
Patrick260 <Patrick260@protonmail.com>
2022-03-12 16:29:59 +0100

    Add that btn_schedule_alarm creates an alarm

--- app/src/main/java/de/patrick260/safealarmclock/activitys/ScheduleAlarmActivity.java
+++ app/src/main/java/de/patrick260/safealarmclock/activitys/ScheduleAlarmActivity.java
@@ -20,8 +20,15 @@ package de.patrick260.safealarmclock.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Random;
 
 import de.patrick260.safealarmclock.R;
+import de.patrick260.safealarmclock.alarm.Alarm;
 
 public final class ScheduleAlarmActivity extends AppCompatActivity {
 
@@ -30,6 +37,26 @@ public final class ScheduleAlarmActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_alarm);
+
+        findViewById(R.id.btn_schedule_alarm).setOnClickListener(this::scheduleAlarm);
+
+    }
+
+    private void scheduleAlarm(final View view) {
+
+        final int alarmId = new Random().nextInt(Integer.MAX_VALUE);
+
+        final Alarm alarm = new Alarm(
+                alarmId,
+                ((EditText) findViewById(R.id.et_alarm_name)).getText().toString(),
+                ((TimePicker) findViewById(R.id.tp_time_picker)).getHour(),
+                ((TimePicker) findViewById(R.id.tp_time_picker)).getMinute(),
+                ((CheckBox) findViewById(R.id.cb_recurring_alarm)).isChecked()
+        );
+
+        alarm.schedule(getApplicationContext());
+
+        finish();
 
     }
 
